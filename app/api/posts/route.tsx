ef75d2251ee59561c6f89d44a87e139f26f23ca9
unknown <achsahal2004@gmail.com>
2025-08-15 08:19:21 +0700

    tugas hari-9

--- app/api/posts/route.tsx
+++ app/api/posts/route.tsx
@@ -3,12 +3,12 @@ import { NextResponse } from 'next/server'
 export async function GET(req: Request) {
     const { searchParams} = new URL(req.url)
     console.log('req => ', searchParams.get('name'))
-return NextResponse.json({ message: 'Help!' }, { status: 200})
+return NextResponse.json({ message: 'Heloo!' }, { status: 200})
 }
 export async function POST(req: Request) {
     const body = await req.json()
     return NextResponse.json({ payload: body }, { status: 200})
-}
+} 
 /*export async function PUT(request: Request) {
 }   
 export async function DELETE(request: Request) {
